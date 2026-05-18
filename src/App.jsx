9f179a7e151d3e9c760052c14c7c5cdcfb5774a1
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-05-19 01:01:28 +0200

    creata funzione per cercare i film

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { useState, useEffect } from "react";
 import Navbar from "./assets/components/navbar";
 function App() {
 
   function handleSearch(query) {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovie(data.results);
+      });
+
     console.log("ricerca effettuata", query);
   }
 
+  const [movie, setMovie] = useState([]);
+
+  console.log(movie);
+  
+
   const apiKey = import.meta.env.VITE_API_KEY;
 
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=ritorno+al+futuro`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
   return (
     <>
       <Navbar onSearch={handleSearch}/>
