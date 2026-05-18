a4e00f4db1be3257fdfb9c26ca7f27f91ef2123a
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-05-18 17:39:49 +0200

    - Creaato file .env - Aggiunto import a bootstrap in main.jsx - Controllo della fetch in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
 function App() {
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=ritorno+al+futuro`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
 }
 export default App
