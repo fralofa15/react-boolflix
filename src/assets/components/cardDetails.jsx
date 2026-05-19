70440a8dd6a0dda7c3e1b2faf64ba7208b33e460
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-05-19 16:32:50 +0200

    - Aggiunta valutazione a stelle in cardDetails - Aggiunti dettagli: Lingua, Titolo originale

--- src/assets/components/cardDetails.jsx
+++ src/assets/components/cardDetails.jsx
@@ -1,10 +1,34 @@
 function CardDetails({ movie }) {
+    const voteFromZeroToFive = movie.vote_average / 2;
+
+    function renderStars() {
+        const stars = [];
+
+        for (let starNumber = 1; starNumber <= 5; starNumber++) {
+            const isFullStar = voteFromZeroToFive >= starNumber;
+            const isHalfStar = voteFromZeroToFive >= starNumber - 0.5;
+
+            if (isFullStar) {
+                stars.push(<i key={starNumber} className="bi bi-star-fill text-warning"></i>);
+            } else if (isHalfStar) {
+                stars.push(<i key={starNumber} className="bi bi-star-half text-warning"></i>);
+            } else {
+                stars.push(<i key={starNumber} className="bi bi-star text-warning"></i>);
+            }
+        }
+
+        return stars;
+    }
+
     return (
         <div className="card-body text-white">
             <h5>{movie.title}</h5>
-            <p>Voto: {movie.vote_average}</p>
+            <p><strong>Titolo originale:</strong> {movie.original_title}</p>
+            <p><strong>Lingua:</strong> {movie.original_language}</p>
+            <p><strong>Voto:</strong> {renderStars()}</p>
             <p>{movie.overview}</p>
         </div>
     );
 }
+
 export default CardDetails;
